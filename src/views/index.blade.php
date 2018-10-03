b0c9b989b5b2a4e4737cbef60fc3c41a8f92c004
Armand Grundmanis <agrundmanis@inbox.lv>
2018-10-03 23:43:42 +0300

    Removed container

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -3,38 +3,33 @@
 @include('laracms.dashboard::partials.summernote')
 
 @section('content')
+    <form method="post">
+        {{ csrf_field() }}
 
-    <div class="content">
-        <div class="container-fluid">
-            <form method="post">
-                {{ csrf_field() }}
-
-                <div class="form-group row">
-                    <label for="to" class="col-md-4 col-form-label text-md-right">{{ __('form.to') }}</label>
-                    <div class="col-md-6">
-                        <select class="form-control" name="to" id="to">
-                            <option value="buyers">Buyers</option>
-                            <option value="sellers">Sellers</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label for="subject" class="col-md-4 col-form-label text-md-right">{{ __('form.subject') }}</label>
-                    <div class="col-md-6">
-                        <input type="text" class="form-control" id="subject" name="subject">
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label for="message" class="col-md-4 col-form-label text-md-right">{{ __('form.message') }}</label>
-                    <div class="col-md-6">
-                        <textarea class="form-control summernote" id="message" name="message"></textarea>
-                    </div>
-                </div>
-
-                <button type="submit" class="btn btn-primary">Send</button>
-            </form>
+        <div class="form-group row">
+            <label for="to" class="col-md-4 col-form-label text-md-right">{{ __('form.to') }}</label>
+            <div class="col-md-6">
+                <select class="form-control" name="to" id="to">
+                    <option value="buyers">Buyers</option>
+                    <option value="sellers">Sellers</option>
+                </select>
+            </div>
         </div>
-    </div>
+
+        <div class="form-group row">
+            <label for="subject" class="col-md-4 col-form-label text-md-right">{{ __('form.subject') }}</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" id="subject" name="subject">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="message" class="col-md-4 col-form-label text-md-right">{{ __('form.message') }}</label>
+            <div class="col-md-6">
+                <textarea class="form-control summernote" id="message" name="message"></textarea>
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Send</button>
+    </form>
 @endsection
